e8cb979e00e9d8d9966e8bf6bbca300a50b24cd8
George Kaimakis <geoptus@gmail.com>
2018-02-26 22:55:45 +0000

    Adds python script

---
+++ led_blinker.py
@@ -0,0 +1,25 @@
+"""Uses the GPIO library to access and control the io of the raspberry pi"""
+
+import RPi.GPIO as GPIO
+import time
+
+
+# Configure the PIN 8:
+GPIO.setmode(GPIO.BOARD)
+GPIO.seup(8, GPIO.OUT)
+GPIO.setwarnings(False)
+
+
+# Blink Interval:
+blink_interval = 0.5  # Seconds
+
+# Blinker Loop:
+while True:
+    GPIO.output(8, True)
+    time.sleep(blink_interval)
+    GPIO.output(8, False)
+    time.sleep(blink_interval)
+
+
+# Release Resources:
+GPIO.cleanup()
